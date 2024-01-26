ed7e69960c121f958812a84bd1daf7d903234398
IWiltshire <122629613+IWiltshire@users.noreply.github.com>
2024-01-26 14:15:11 +0000

    Update rectVertex.glsl

--- lwjgl-camera-first-person/src/rectVertex.glsl
+++ lwjgl-camera-first-person/src/rectVertex.glsl
@@ -1,6 +1,5 @@
 #version 330 core
 layout (location=0) in vec3 aPos;
-//layout (location=1) in vec3 aColour;
 layout (location=1) in vec2 aTexCoord;
 
 out vec3 ourColour;
@@ -14,8 +13,5 @@ uniform mat4 projection;
 void main()
 {
     gl_Position = projection * view * model * vec4(aPos, 1.0); // correct order
-    //gl_Position = projection * view * model * vec4(aPos, 1.0); // correct order
-    //gl_Position = vec4(aPos, 1.0);
-    //ourColour = aColour;
     TexCoord = aTexCoord;
 }
